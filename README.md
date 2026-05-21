6c470dc75a40f52c17b7c79f24737800d387b417
Your Name <franzluehr@gmail.com>
2026-05-21 12:35:47 +0200

    docs: auto-sync v6.8.0

--- README.md
+++ README.md
@@ -30,3 +30,8 @@ push-all-addons
 ```
 
 
+
+## Used By
+1 other ChillCube addon(s) depend on this:
+
+- [InventoryFrame2D](https://github.com/ChillCube/InventoryFrame2D)
--- sprite_placement.gd
+++ sprite_placement.gd
@@ -4,9 +4,11 @@ class_name SpritePlacement2D
 @onready var placement : PlacementArea2D = PlacementArea2D.new();
 @onready var collision_shape : CollisionShape2D = CollisionShape2D.new()
 @onready var shape : RectangleShape2D = RectangleShape2D.new();
+@export var accepted_classes : Array[String] = ["PlaceableItem2D"];
 
 func _ready() -> void:
 	placement.add_child(collision_shape)
+	placement.accepted_classes = accepted_classes
 	collision_shape.shape = shape;
 	shape.size = SpriteHelper.get_size(self)
 	add_child(placement);
